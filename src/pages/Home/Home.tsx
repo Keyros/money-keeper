a90d8eb17f876997fe0cb0d32cba2144561fa0b3
Rasim Keita <keita.rasim@gmail.com>
2024-08-03 03:38:46 +0400

    Refactor project structure and update app configuration
    Reorganized the project structure by moving components to the pages directory and updating import paths. Updated configurations and static assets, including changes to `tsconfig` files, `package.json`, and the main HTML file. Added a new `Home` page component and removed unnecessary assets.

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home page of Money Keeper</h1>
+            <p>Here you will find an overview of all your financial transactions</p>
+        </div>
+    );
+};
