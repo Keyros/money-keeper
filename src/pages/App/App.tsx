4c313577415d85e35589f3c0117e75f0cd2c81dd
viktarpunko <punko89viktor@gmail.com>
2024-08-03 04:36:47 +0400

    Env file example

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,7 +1,18 @@
 import './App.css'
 import {Home} from "@pages/Home/Home.tsx";
+import eruda from 'eruda';
+import {useEffect} from "react";
+
+
 
 const App = () => {
+
+    useEffect(() => {
+        console.log('App render', import.meta.env.MODE);
+        if (import.meta.env.MODE === 'development') {
+            eruda.init();
+        }
+    }, []);
 
     return (
         <>
